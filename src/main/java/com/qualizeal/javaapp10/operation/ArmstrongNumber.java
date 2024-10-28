ba5249b511c92c1a902f5a80f69d6af868cf15a8
vijaya lakshmi <dasarisaibalaji2006@gmail.com>
2024-10-28 18:06:38 +0530

    Amstrog Code Solution is added

---
+++ src/main/java/com/qualizeal/javaapp10/operation/ArmstrongNumber.java
@@ -0,0 +1,41 @@
+package com.qualizeal.javaapp10.operation;
+
+public class ArmstrongNumber {
+
+	public String defineArmstrongNumber() {
+		return "For a given number of digits 𝑛 n is a number that is equal to the sum of its own digits each raised to the power of 𝑛 n.";
+	}
+
+	public String verifyIfNumberIsArmstrongOrNot(int num1) {
+		int temp = num1, c = 0, temp1 = num1;
+		if (num1 < 0) {
+			return "-1";
+		}
+		if (num1 == 0) {
+			return "-2";
+		}
+		while (temp != 0) {
+			c = c + 1;
+			temp = temp / 10;
+		}
+		int sum = 0, rem;
+		while (temp1 != 0) {
+			rem = temp1 % 10;
+			int ams = 1;
+//			System.out.println(rem);
+			for (int i = 0; i < c; i++) {
+				ams = ams * rem;
+			}
+//			System.out.println(ams);
+			sum = sum + ams;
+			temp1 = temp1 / 10;
+		}
+//		System.out.println(sum);
+		if (sum == num1) {
+			return "Armstrong Number";
+		} else {
+			return "Not an Armstrong Number";
+		}
+	}
+
+}
