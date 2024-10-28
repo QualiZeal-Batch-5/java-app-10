fc8afae11571d315f100439d5bf5ec0f4157984b
Chudamani Charitha <nuguricharitha@gmail.com>
2024-10-28 18:24:16 +0530

    Added a new file.

--- src/main/java/com/qualizeal/App.java
+++ src/main/java/com/qualizeal/App.java
@@ -1,7 +1,10 @@
 package com.qualizeal;
 
+import com.qualizeal.javaapp10.operation.ArmstrongNumber;
+
 public class App {
 	public static void main(String[] args) {
-		
+		ArmstrongNumber armstrongNumber = new ArmstrongNumber();
+		armstrongNumber.verifyIfNumberIsArmstrongOrNot(153);
 	}
 }
