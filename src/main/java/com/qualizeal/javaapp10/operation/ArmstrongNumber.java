ee327a9779ee224d9f0ebbdfae929785c49248bb
Dandu Srija <dandusrija2503@gmail.com>
2024-10-28 19:03:52 +0530

    Performed an operation to identify if a particular number is an ARMSTRONG NUMBER or not.

---
+++ src/main/java/com/qualizeal/javaapp10/operation/ArmstrongNumber.java
@@ -0,0 +1,50 @@
+package com.qualizeal.javaapp10.operation;
+ 
+public class ArmstrongNumber {
+	public String defineArmstrongNumber() {
+		return "Armstrong Number.";
+	}
+ 
+	public String verifyIfNumberIsArmstrongOrNot(int num) {
+		
+		int number = num;
+		int result = 0;
+		int rem = 1;
+		int res = 1;
+		int count = 0;
+		int cnt = 0;
+ 
+		if (num == 0)
+			return "-2";
+		else if (num < 0)
+			return "-1";
+		else {
+			while (num != 0) {
+				num = num / 10;
+				count++;
+			}
+ 
+			cnt = count;
+			num = number;
+ 
+			while (num != 0) {
+				rem = num % 10;
+ 
+				while (cnt != 0) {
+					res = res * rem;
+					cnt--;
+				}
+ 
+				result += res;
+				cnt = count;
+				num = num / 10;
+				res = 1;
+			}
+ 
+			if (result == number)
+				return "Armstrong Number";
+			else
+				return "Not an Armstrong Number";
+		}
+	}
+}
