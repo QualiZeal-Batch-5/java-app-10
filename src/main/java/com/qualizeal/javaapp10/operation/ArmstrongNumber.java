170ff5374f9a30ff7dfe57a8b7dc53c11dbc5552
PurnaAnde <purnaande.3101@gmail.com>
2024-10-31 12:40:45 +0530

    ArmStrong Solution Added.

--- src/main/java/com/qualizeal/javaapp10/operation/ArmstrongNumber.java
+++ src/main/java/com/qualizeal/javaapp10/operation/ArmstrongNumber.java
@@ -1,45 +1,39 @@
 package com.qualizeal.javaapp10.operation;
 
 public class ArmstrongNumber {
-	public String defineArmstrongNumber() {
-		return "Given Number is equal to the sum of its own digits each raised to the power of the number of digits.";
+	public void defineArmstrongNumber() {
+		System.out.println("Number that is equal to the sum of its digits, each raised to the power of the number of digits");
 	}
-
-	public String verifyIfNumberIsArmstrongOrNot(int number1) {
-
-		if (number1 < 0) {
+	public String verifyIfNumberIsArmstrongOrNot(int num) {
+		if (num <0) {
 			return "-1";
-		} else if (number1 == 0) {
+		}
+		else if (num ==0) {
 			return "-2";
-		} else {
-			int temp = number1;
-			int numOfDigits = 0;
-
-			while (temp > 0) {
-				temp /= 10;
-				numOfDigits++;
-			}
-
-			temp = number1;
-			int sum = 0;
-
-			while (temp > 0) {
-				int digit = temp % 10;
-				int power = 1;
-
-				for (int i = 0; i < numOfDigits; i++) {
-					power *= digit;
+		}
+		else {
+			int temp=num;
+			int l = String.valueOf(num).length();
+			int sum=0;
+			while(temp!=0) {
+				int rem = temp %10;
+				
+				int power=1;
+				for(int i=0; i<l; i++) {
+					power = power * rem ;
 				}
-
-				sum += power;
-				temp /= 10;
+				temp = temp /10;
+				sum = sum + power ;
+				
 			}
-
-			if (sum == number1) {
+			if (num == sum) {
 				return "Armstrong Number";
-			} else {
+			}
+			else {
 				return "Not an Armstrong Number";
 			}
 		}
+		
 	}
+
 }
